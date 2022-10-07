f2974b2398b7b7c010fe97c49fc3b684f0c9db25
IvanJMartinezS <ivanmartinezwork@gmail.com>
2022-10-06 23:05:58 -0400

    Estructura Basica

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Venta de Entradas</title>
+    <link rel="stylesheet" href="css/styles.css" type="text/css" />
+</head>
+<body>
+    <div id="container">
+        <div id="header">
+            Header
+        </div>
+        <div id="scenery">
+            Scenery
+        </div>
+        <div id="information">
+            Information
+        </div>
+        <div id="footer">
+            Footer
+        </div>
+    </div>
+</body>
+</html>
